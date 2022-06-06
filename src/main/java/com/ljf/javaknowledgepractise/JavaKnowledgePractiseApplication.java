392712cd4d3484cdd7883be504d1c2bd3c7fd6c8
李佳帆 <3151342895@qq.com>
2022-06-06 14:37:05 +0800

    Initial

---
+++ src/main/java/com/ljf/javaknowledgepractise/JavaKnowledgePractiseApplication.java
@@ -0,0 +1,13 @@
+package com.ljf.javaknowledgepractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKnowledgePractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaKnowledgePractiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljf/javaknowledgepractise/JavaKnowledgePractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljf.javaknowledgepractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKnowledgePractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

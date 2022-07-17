05fb413351d037827c652d0e4ebfc076199a0474
李佳帆 <3151342895@qq.com>
2022-07-17 13:35:04 +0800

    string constant pool test

--- src/main/java/com/ljf/javaknowledgepractise/stringtest/test.java
+++ src/main/java/com/ljf/javaknowledgepractise/stringtest/test.java
@@ -2,21 +2,19 @@ package com.ljf.javaknowledgepractise.stringtest;
 
 public class test {
     public static void main(String[] args) {
-        String c = "12";
         String a = "1";
         String b = new String(a + "2");
-        System.out.println(b == c);
-        b = b.intern();
+        b.intern();
+        String c = "12";
         System.out.println(b == c);
 
         System.out.println("======");
 
         String aa = "a";
         String bb = new String(aa + "b");
-        String dd = bb.intern();
+        bb.intern();
         String cc = "ab";
         System.out.println(bb == cc);
-        System.out.println(dd == cc);
 
         System.out.println("======");
 
